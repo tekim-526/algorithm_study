9d7a71f3b6c713e312c96989a854e9d6be8f2227
tekim <rldod526@naver.com>
2022-03-04 17:54:05 +0900

    boj-10824

---
+++ 백준/10824-네 수/10824-네 수/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  10824-네 수
+//
+//  Created by Kim TaeSoo on 2022/03/04.
+//
+
+import Foundation
+
+let a = readLine()!.components(separatedBy: " ")
+
+print(Int(a[0] + a[1])! + Int(a[2] + a[3])!)
